ad2e168e55f2f3076f1f4c428d6f9177140c0310
sarah <89361834+sweetdemis@users.noreply.github.com>
2023-05-09 11:52:07 -0300

    Update and rename app.js to trabalho/app.js

--- trabalho/app.js
+++ trabalho/app.js
@@ -7,7 +7,20 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', "ejs")
 app.set('views', path.join(__dirname, 'views'))
 
+app.get('/detalhe', (req, res) => {
+   
+    const detalhes = {
+        livro1: "detalhe",
+        livro2: "detalhe",
+        livro3: "detalhe"
+    }
+
+    const detalhe = req.params;
+    res.render('detalhes', {detalhe, detalhes})
+})
+
 app.get('/', (req, res) => {
+
     const livros = {
         livro1: "A livraria dos Achados e Perdidos",
         livro2: "Cool For The Summer: Um Verão Insquecível",
@@ -16,15 +29,6 @@ app.get('/', (req, res) => {
     res.render('home', {livros});
 })
 
-app.get('/detalhe/:nome', (req, res) => {
-    const detalhes = {
-        livro1: "detalhe",
-        livro2: "detalhe",
-        livro3: "detalhe"
-    }
-    res.render('detalhe', {livros, detalhes});
-})
-
-app.listen(3000, () => {
-    console.log("servidor ligado na porta 3000!")
+app.listen(3000, () =>{
+    console.log("servidor ligado na porta 3000");
 })
