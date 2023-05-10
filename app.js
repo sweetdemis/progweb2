67b76b5c2411320fee5eaa447bcb0e4efbd8975b
sarah <89361834+sweetdemis@users.noreply.github.com>
2023-05-10 10:08:48 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ app.set('views', path.join(__dirname, 'views'))
 app.get('/detalhe', (req, res) => {
    
     const detalhes = {
-        livro1: "detalhe",
-        livro2: "detalhe",
-        livro3: "detalhe"
+        livro1: "Uma história sobre recomeços, o amor e a paixão pelos livros, conheça o romance contemporâneo que conquistou milhares de leitores da best-seller Susan Wiggs, autora de Mapa do coração e Clube de Costura dos Corações Remendados.",
+        livro2: "Uma história sobre autodescoberta e novos amores: O cara dos sonhos... ou a garota que não sai da sua cabeça?",
+        livro3: "Neste clássico da literatura, e um dos mistérios mais famosos da Rainha do Crime, Hercule Poirot precisa descobrir quem está por trás do assassinato no Expresso do Oriente – e o culpado está entre os passageiros do trem."
     }
 
     const detalhe = req.params;
