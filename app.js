e5506da4afc6abdc53d29dffb1156ce75cffb1d6
sarah <89361834+sweetdemis@users.noreply.github.com>
2023-05-11 12:48:40 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', "ejs")
 app.set('views', path.join(__dirname, 'views'))
 
-app.get('/detalhe', (req, res) => {
+app.get('/detalhe/:livro', (req, res) => {
    
     const detalhes = {
         livro1: "Uma história sobre recomeços, o amor e a paixão pelos livros, conheça o romance contemporâneo que conquistou milhares de leitores da best-seller Susan Wiggs, autora de Mapa do coração e Clube de Costura dos Corações Remendados.",
@@ -15,16 +15,31 @@ app.get('/detalhe', (req, res) => {
         livro3: "Neste clássico da literatura, e um dos mistérios mais famosos da Rainha do Crime, Hercule Poirot precisa descobrir quem está por trás do assassinato no Expresso do Oriente – e o culpado está entre os passageiros do trem."
     }
 
-    const detalhe = req.params;
-    res.render('detalhes', {detalhe, detalhes})
+    const titulos = {
+        livro1: "Vermelho, Branco e Sangue Azul",
+        livro2: "Baía da Esperança",
+        livro3: "É Assim que Acaba"
+    }
+
+    const imagens = {
+        livro1: "/img/IMG-3281.jpg",
+        livro2: "/img/IMG-3282.jpg",
+        livro3: "/img/IMG-3283.jpg"
+    }
+
+    const {livro} = req.params;
+    const titulo = titulos[livro];
+    const detalhe = detalhes[livro];
+    const imagem = imagens[livro];
+    res.render('detalhe', {titulo, detalhe, imagem})
 })
 
 app.get('/', (req, res) => {
 
     const livros = {
-        livro1: "A livraria dos Achados e Perdidos",
-        livro2: "Cool For The Summer: Um Verão Insquecível",
-        livro3: "Assassinto no Expresso do Oriente"
+        livro1: "Vermelho, Branco e Sangue Azul",
+        livro2: "Baía da Esperança",
+        livro3: "É Assim que Acaba"
     }
     res.render('home', {livros});
 })
